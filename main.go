fcc5fe84fa9a8ce4ea1e68fbbe39a7eda7e9b89b
GuHang <499809640@qq.com>
2023-05-14 15:46:57 +0800

    连接数据库和Viper

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "xiangxiang/jackaroo/app/huawei"
+import (
+	"database/sql"
+	"xiangxiang/jackaroo/app/huawei"
+	"xiangxiang/jackaroo/global"
+	"xiangxiang/jackaroo/inital"
+)
 
 func main() {
+	global.G_VP = inital.Viper()
+	global.G_DB = inital.Gorm()
+	if global.G_DB != nil {
+		db, _ := global.G_DB.DB()
+		defer func(db *sql.DB) {
+			db.Close()
+		}(db)
+	}
+
 	huawei.Handler("")
 }
