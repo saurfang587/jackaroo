d13950dce016781a3c48aecaf55f8434c78e3840
Jing-Hua1 <78458471+Jing-Hua1@users.noreply.github.com>
2023-05-20 10:13:54 +0800

    更新：定时器1

---
+++ router/router.go
@@ -0,0 +1,152 @@
+package router
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"sync"
+	"time"
+	"xiangxiang/jackaroo/app/Alibaba"
+	"xiangxiang/jackaroo/app/Baidu"
+	"xiangxiang/jackaroo/app/Meituan"
+	"xiangxiang/jackaroo/app/Tencent"
+	"xiangxiang/jackaroo/app/Wangyi"
+	"xiangxiang/jackaroo/app/Weiruan"
+	Bilibili "xiangxiang/jackaroo/app/bilibili"
+	"xiangxiang/jackaroo/app/jingdong"
+	"xiangxiang/jackaroo/app/mihoyo"
+	"xiangxiang/jackaroo/global"
+)
+
+func Router() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		Hello()
+	})
+	err1 := r.Run()
+	if err1 != nil {
+		fmt.Println("引擎启动失败", err1)
+	}
+	//定时删除未更新的数据：即招聘公司已经撤销的招聘信息
+	Alibaba.DeleteStaleRecords(global.G_DB)
+}
+
+// 并发爬取信息
+func Hello() {
+	var wg sync.WaitGroup
+	wg.Add(9)
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Alibaba.Header(""); pan != false {
+				fmt.Println("alibaba爬完了")
+				break
+			} else {
+				fmt.Println("alibaba重试", err)
+				time.Sleep(3 * time.Second)
+			}
+
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Baidu.Header(""); pan != false {
+				fmt.Println("百度爬完了")
+				break
+			} else {
+				fmt.Println("百度重试", err)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Bilibili.Header(""); pan != false {
+				fmt.Println("Bilibili爬完了")
+				break
+			} else {
+				fmt.Println("bilibili重试", err)
+				time.Sleep(3 * time.Second)
+			}
+
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := jingdong.Header(""); pan != false {
+				fmt.Println("jingdong爬完了")
+				break
+			} else {
+				fmt.Println("京东重试", err)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Meituan.Header(""); pan != false {
+				fmt.Println("美团爬完了")
+				break
+			} else {
+				fmt.Println("美团重试", err)
+				time.Sleep(3 * time.Second)
+				continue
+			}
+
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := mihoyo.Header(""); pan != false {
+				fmt.Println("米哈游爬完了")
+				break
+			} else {
+				fmt.Println("米哈游重试", err)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Tencent.Header(""); pan != false {
+				fmt.Println("腾讯爬完了")
+				break
+			} else {
+				fmt.Println("腾讯重试", err)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Wangyi.Header(""); pan != false {
+				fmt.Println("网易爬完了")
+				break
+			} else {
+				fmt.Println("网易重试", err)
+				time.Sleep(3 * time.Second)
+			}
+
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if pan, err := Weiruan.Header(""); pan != false {
+				fmt.Println("微软爬完了")
+				break
+			} else {
+				fmt.Println("微软重试", err)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+	wg.Wait()
+	fmt.Println("所有网站都爬取完毕了")
+}
